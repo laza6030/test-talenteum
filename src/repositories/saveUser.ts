fce690ae883d793df7de84019e8fc9342168c8a5
Laza Nantenaina <laza@hairun-technology.com>
2024-07-26 23:08:36 +0300

    Save user

--- src/repositories/saveUser.ts
+++ src/repositories/saveUser.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from "express";
 
+import { pool } from "../database";
 import { isEmailValid } from "../helpers";
 import { User } from "../types";
 
-export const saveUser = (req: Request, res: Response) => {
+export const saveUser = async (req: Request, res: Response) => {
   const user: User = req.body;
 
   if (!user.username.trim()) throw new Error("Username can not be empty");
@@ -12,6 +13,19 @@ export const saveUser = (req: Request, res: Response) => {
     throw new Error("Please provide a valid email");
 
   if (user.age < 0) throw new Error("Age can not be negative");
+
+  try {
+    // Save the user to the database
+    await pool.query(
+      `
+      INSERT INTO users (username, email, age) VALUES ($1, $2, $3);
+    `,
+      [user.username, user.email, user.age]
+    );
+  } catch (error) {
+    console.error("Error saving user", error);
+    throw new Error("Error saving user");
+  }
 
   res.send("User saved successfully");
 };
